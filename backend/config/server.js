d3bfa18528611885a47dddbf4a5c5d810cf28121
Ricardo Do Vale <pataco80@gmail.com>
2022-02-10 15:31:26 +0100

    add backend strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ff97dcde44bbf1c06657498e8445693'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e24195f-55b6-4040-bea3-573d6b0398d5'
+};
